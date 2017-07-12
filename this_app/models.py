c4880ef0350d7cf58d9dff344f98f3b3096f2613
borenho <borecollins@gmail.com>
2017-07-12 12:07:32 +0300

    Update User model class with flask-login methods

--- this_app/models.py
+++ this_app/models.py
@@ -1,8 +1,10 @@
+from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(object):
-    """represents a user who can CRUD his own bucketlists"""
+class User(UserMixin):
+    """Represents a user who can Create, Read, Update & Delete his own bucketlists"""
 
+    counter = 0
     users = {}
 
     def __init__(self, email, username, password):
@@ -10,14 +12,15 @@ class User(object):
 
         self.email = email
         self.username = username
-        self.password = generate_password_hash(password)
+        self.password = password
+        User.counter += 1
 
 
     def create_user(self):
         """ Class to create and store a user object """
 
         self.users.update({
-            self.email: {
+            self.counter: {
                 'email': self.email,
                 'username': self.username,
                 'password': self.password
@@ -25,3 +28,19 @@ class User(object):
         })
 
         return self.users
+
+    def is_active(self):
+        """True, as all users are active."""
+        return True
+
+    def get_id(self):
+        """Return the email address to satisfy Flask-Login's requirements."""
+        return self.email
+
+    def is_authenticated(self):
+        """Return True if the user is authenticated."""
+        return True
+
+    def is_anonymous(self):
+        """False, as anonymous users aren't supported."""
+        return False
