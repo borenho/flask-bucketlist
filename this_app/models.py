6458dc035ba1b6b49469cae6f7273306cd0befd0
borenho <borecollins@gmail.com>
2017-07-13 15:41:50 +0300

    Add class Bucketlist

--- this_app/models.py
+++ this_app/models.py
@@ -1,5 +1,3 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
 class User(object):
     """Represents a user who can Create, Read, Update & Delete his own bucketlists"""
 
@@ -9,10 +7,11 @@ class User(object):
     def __init__(self, email, username, password):
         """Constructor class to initialize class"""
 
+        User.counter += 1
         self.email = email
         self.username = username
         self.password = password
-        User.counter += 1
+        self.bucketlists = {}
 
 
     def create_user(self):
@@ -23,6 +22,34 @@ class User(object):
                 'email': self.email,
                 'username': self.username,
                 'password': self.password
+            }
+        })
+
+        return self
+
+
+class Bucketlist(object):
+    """Represents a user who can Create, Read, Update & Delete his own bucketlists"""
+
+    counter = 0
+    bucketlists = {}
+
+    def __init__(self, name, description):
+        """Constructor class to initialize class"""
+
+        Bucketlist.counter += 1
+        self.name = name
+        self.description = description
+        self.bucketlist_items = {}
+
+
+    def create_bucketlist(self):
+        """ Class to create and store a user object """
+
+        self.bucketlists.update({
+            self.counter: {
+                'name': self.name,
+                'description': self.description
             }
         })
 
